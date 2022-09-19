69fb3814eaed5cef3429a89e9e8b9f13339e0678
jayGrit <slowt0425@gmail.com>
2022-09-19 17:58:39 +0900

    be 001

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ const app = express();
 const port = 8000;
 
 app.set("view engine", "ejs");
+app.use("/static", express.static("static"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get("/", function (req, res) {
   res.render("form");
 });
+
+const router = require("./routes");
+app.use('/', router);
 
 app.listen(port, function () {
   console.log("Server open :", port);
---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+// const user = require("../controller/UserController");
+
+// router.get("/", user.index);
+
+// router.get("/signup", user.signup);
+// router.post("/signup", user.post_signup);
+
+// router.get("/signin", user.signin);
+// router.post("/signin", user.post_signin);
+
+// router.post("/profile", user.profile);
+// router.post("/profile/edit", user.profile_edit);
+// router.post("/profile/delete", user.profile_delete);
+
+module.exports = router;
