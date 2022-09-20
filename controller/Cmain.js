bf5a55cbeb0dda8d3866555e7a3065df41c1bf1b
jayGrit <slowt0425@gmail.com>
2022-09-20 16:29:33 +0900

    signup be

--- controller/Cmain.js
+++ controller/Cmain.js
@@ -5,3 +5,10 @@ exports.getJoinMember = (req, res) => {
     res.render("joinmember");
 }
 // -------------------------------
+
+exports.postJoinMember = (req, res) => {
+    console.log(req.body);
+    Main.postJoinMember(req.body, function(result){
+        res.send(true);
+    });
+}
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const main = require("../controller/Cmain")
 const user = require("../controller/Cuser");
 
 router.get("/joinMember", main.getJoinMember);
+router.post("/joinMember/user", main.postJoinMember)
 
 router.get("/introduce", user.getIntroduce);
 
