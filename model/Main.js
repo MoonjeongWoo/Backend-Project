0b05bebcfaf5f236788f454f76a36119f0f6ba32
jayGrit <slowt0425@gmail.com>
2022-09-20 18:51:07 +0900

    idcheck be

--- model/Main.js
+++ model/Main.js
@@ -1,12 +1,29 @@
-const {cnn} = require("./index");
+const cnn = require("./index");
+
+exports.idCheck = (data, cb) => {
+    var sql = `SELECT * FROM users Where id = "${data.id}"`;
+
+    cnn.query( sql, function(err, rows) {
+        // console.log( "결과값길이", rows.length );
+        // console.log( "결과값", rows );
+        if  (rows.length === 0) {
+            cb({result: 0});         
+        } else  {
+            cb({result: 1});
+        };
+        if ( err ) throw err;
+    });
+}
+
 
 exports.postJoinMember = (data, cb) => {
-    // var uuid = ;
-    // unhex()
-    let sql = `INSERT INTO users VALUES("1", '${data.id}','${data.pw}','${data.name}','${data.email}','${data.location}');`;
-    console.log(sql);
-    // cnn.query( sql, function(err, rows){
-    //     if ( err ) throw err;
-    //     cb(rows);
-    // });
+    // let sql = `select * from users`;
+    let sql = `INSERT INTO users VALUES(unhex(replace(uuid(),'-','')), '${data.id}','${data.pw}','${data.name}','${data.email}','${data.location}');`;
+    // console.log(sql);
+    cnn.query( sql, function(err, rows){
+        console.log(rows);
+        if ( err ) throw err;
+        cb(rows);
+    });
+    
 }
