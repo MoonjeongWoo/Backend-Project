1df6bb8fbfc6fed3f9577669d000225d9d78a76d
norii97 <mbhb8817@gmail.com>
2022-09-20 14:49:39 +0900

    join page

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const main = require("../controller/Cmain")
 const user = require("../controller/Cuser");
 
-router.get("/", main.getMain);
+router.get("/joinMember", main.getJoinMember);
 
 router.get("/introduce", user.getIntroduce);
 
