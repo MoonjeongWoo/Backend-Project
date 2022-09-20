6d8809179a236d22da3b04d2a43c815b285b022e
jayGrit <slowt0425@gmail.com>
2022-09-20 17:00:28 +0900

    signup be 2

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const session = require("express-session");
 
 app.set("view engine", "ejs");
 app.use("/static", express.static("static"));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 const router = require("./routes");
--- controller/Cmain.js
+++ controller/Cmain.js
@@ -2,7 +2,8 @@ const Main = require("../model/Main");
 
 // get joinmember page
 exports.getJoinMember = (req, res) => {
-    res.render("login");
+    // res.render("login");
+    res.render("joinmember");
 }
 // -------------------------------
 
