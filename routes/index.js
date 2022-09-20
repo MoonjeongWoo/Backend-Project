c8533e3e08535e29b8414176abc451f1b3c9a85f
norii97 <mbhb8817@gmail.com>
2022-09-20 13:23:38 +0900

    9/20

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,6 @@ const user = require("../controller/UserController");
 
 router.get("/", user.index);
 
-router.get("/signup", user.signup);
-router.post("/signup", user.post_signup);
 
-router.get("/signin", user.signin);
-router.post("/signin", user.post_signin);
-
-router.post("/profile", user.profile);
-router.post("/profile/edit", user.profile_edit);
-router.post("/profile/delete", user.profile_delete);
-
-
-app.get("/", function (req, res) {
-    res.render("form");
-});
 
 module.exports = router;
