186bb455131417439dc068e13f07500795398036
jeongwoo <mbh219@naver.com>
2022-09-21 18:58:08 +0900

    1st commit

--- controller/Cmain.js
+++ controller/Cmain.js
@@ -30,34 +30,32 @@ exports.postJoinMember = (req, res) => {
 exports.userLogin = (req, res) => {
   Main.userLogin(req.body, function (result, userId) {
     if (result != 0) {
-      console.log('userid', userId)
+      console.log("userid", userId);
       if (!req.session.user) {
-        Main.getUuid(userId, function(uuid) {
+        Main.getUuid(userId, function (uuid) {
           req.session.user = uuid;
-          console.log("uuid", uuid)
-        })
+          console.log("uuid", uuid);
+        });
       } else {
-        console.log('session 이미 있음')
+        console.log("session 이미 있음");
       }
-      var data = { result: 1 }
+      var data = { result: 1 };
     } else {
-      var data = { result: 0 }
+      var data = { result: 0 };
     }
-    
-    // login success return 1 fail return 0 
+
+    // login success return 1 fail return 0
     res.send(data);
-  })
-}
+  });
+};
 
 // id exist check
 exports.idCheck = (req, res) => {
-    Main.idCheck (req.body, function(result){
-        res.send(result);
-        // result 값이 0이면 중복 id 없음, 1 이면 중복 아이디 있음
-    });
+  Main.idCheck(req.body, function (result) {
+    res.send(result);
+    // result 값이 0이면 중복 id 없음, 1 이면 중복 아이디 있음
+  });
 };
-
-
 
 // 시퀄 전환 시작
 
