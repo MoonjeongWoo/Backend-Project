9f8ed9a6199d078b513932ed9ce55ee487cab6ab
jayGrit <slowt0425@gmail.com>
2022-09-21 15:26:35 +0900

    seq 3

--- model/index.js
+++ model/index.js
@@ -12,21 +12,21 @@ module.exports = cnn;
 
 
 // 시퀄 컨버젼 시작.
-const Sequelize = require("sequelize");
-const config = require("../config/config.json")["conv_test"];
+// const Sequelize = require("sequelize");
+// const config = require("../config/config.json")["conv_test"];
 
-const db = {};
+// const db = {};
 
-const sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-);
+// const sequelize = new Sequelize(
+//     config.database,
+//     config.username,
+//     config.password,
+//     config
+// );
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+// db.sequelize = sequelize;
+// db.Sequelize = Sequelize;
 
-db.Main = require("./Main")(sequelize, Sequelize);
+// db.Main = require("./Main")(sequelize, Sequelize);
 
-module.exports = db;
+// module.exports = db;
