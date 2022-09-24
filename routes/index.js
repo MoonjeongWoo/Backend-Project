678bf29e2bdf85f1dd59d5208fcbb4eb8385d904
jeongwoo <mbh219@naver.com>
2022-09-24 19:06:51 +0900

    장바구니에 추가,및, 찜 버튼 추가, 지역 추가

--- routes/index.js
+++ routes/index.js
@@ -8,45 +8,43 @@ const company = require("../controller/Ccompany");
 
 // main
 //---------------------------------------
-    // get main page
-    router.get("/", main.getMain);
+// get main page
+router.get("/", main.getMain);
 
-    // try login
-    router.post("/userLogin", main.userLogin);
-    router.post("/userLoginCompany", main.userLoginCompany);
+// try login
+router.post("/userLogin", main.userLogin);
+router.post("/userLoginCompany", main.userLoginCompany);
 
-    // joim membership
-    router.get("/joinMember", main.getJoinMember);
-    router.post("/joinMember/idcheck", main.idCheck);
-    router.post("/joinMember/user", main.postJoinMember);
+// joim membership
+router.get("/joinMember", main.getJoinMember);
+router.post("/joinMember/idcheck", main.idCheck);
+router.post("/joinMember/user", main.postJoinMember);
 
-    router.get("/joinMemberCompany", main.getJoinMemberCompany);
-    router.post("/joinMemberCompany/idcheck", main.idCheckCompany);
-    router.post("/joinMemberCompany/user", main.postJoinMemberCompany);
+router.get("/joinMemberCompany", main.getJoinMemberCompany);
+router.post("/joinMemberCompany/idcheck", main.idCheckCompany);
+router.post("/joinMemberCompany/user", main.postJoinMemberCompany);
 //---------------------------------------
-
 
 // user
 //---------------------------------------
-    // get introduce
-    router.get("/introduce", userResume.getIntroduce);
-    router.post("/user/saveIntroudce", userResume.saveIntroudce);
+// get introduce
+router.get("/introduce", userResume.getIntroduce);
+router.post("/user/saveIntroudce", userResume.saveIntroudce);
 //---------------------------------------
-
 
 // company
 //---------------------------------------
-    // get company page
-    router.get("/company", company.getCompany);
+// get company page
+router.get("/company", company.getCompany);
 
-    // post stack tag
-    router.post("/sortUserByElement", company.sortUserByElement);
+// post stack tag
+router.post("/sortUserByElement", company.sortUserByElement);
 //---------------------------------------
 
 // myPage
 //---------------------------------------
-    // get myPage
-    router.get("/myPage", user.getMyPage);
+// get myPage
+// router.get("/myPage", user.getMyPage);
 //---------------------------------------
 
 module.exports = router;
