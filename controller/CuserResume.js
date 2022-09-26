b02c563ed67d83286c5f1fd17ef3a66f23eff42d
norii97 <mbhb8817@gmail.com>
2022-09-26 16:27:20 +0900

    model controller 데이터 입력 받기 직전까지 제작 완료

--- controller/CuserResume.js
+++ controller/CuserResume.js
@@ -1,5 +1,7 @@
 const { UserResume } = require("../model");
-
+const {ElementCareer} = require("../model");
+const {ElementLocation} = require("../model");
+const {ElementStack} = require("../model");
 
 // get introduce page
 exports.getIntroduce = (req, res) => {
@@ -16,8 +18,16 @@ exports.saveIntroudce = (req, res) => {
         career: req.body.career,
         portfolio: req.body.range,
         etc: req.body.etc
-    }).then((result) => {
-        // console.log(result)
+    }).then(() => {
+        // ElementCareer.create({
+            
+        // })
+        // ElementLocation.create({
+            
+        // })
+        // ElementStack.create({
+            
+        // })
     })  
 }
 // ----------------
