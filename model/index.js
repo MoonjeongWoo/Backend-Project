8ba46097e4b5037b60bd747dbaa54471dd396021
norii97 <mbhb8817@gmail.com>
2022-09-27 15:47:25 +0900

    model/index add db tables

--- model/index.js
+++ model/index.js
@@ -16,8 +16,12 @@ db.Sequelize = Sequelize;
 
 // db.Main = require("./Main")(sequelize, Sequelize);
 
-db.UserResume = require("./UserResume.js")(sequelize, Sequelize);
-db.UserInfo = require("./UserInfo.js")(sequelize, Sequelize);
-db.CompanyInfo = require("./CompanyInfo.js")(sequelize, Sequelize);
+db.UserResume = require("./UserResume")(sequelize, Sequelize);
+db.CompanyInfo = require("./CompanyInfo")(sequelize, Sequelize);
+db.UserInfo = require("./UserInfo")(sequelize, Sequelize);
+db.ElementCareer = require("./ElementCareer")(sequelize, Sequelize);
+db.ElementLocation = require("./ElementLocation")(sequelize, Sequelize);
+db.ElementStack = require("./ElementStack")(sequelize, Sequelize);
+
 
 module.exports = db;
