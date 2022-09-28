0899f325ad9ddb170d7cbb1ded9324754cf44af2
jeongwoo <mbh219@naver.com>
2022-09-28 13:33:57 +0900

    아무것도 안건들었습니다

--- controller/Cmain.js
+++ controller/Cmain.js
@@ -4,10 +4,10 @@ const { isLogin } = require("./Cfunc");
 
 // get main page
 exports.getMain = (req, res) => {
-  console.log('this', isLogin(req.session.uuid) )
+  console.log("this", isLogin(req.session.uuid));
 
   res.render("main", { isLogin: 1 });
-  console.log('gogo1', req.session.uuid);
+  console.log("gogo1", req.session.uuid);
   //찍으면 undifined 생김
   // res.render("main", {isLogin: isLogin});
 };
@@ -78,7 +78,7 @@ exports.userLogin = (req, res) => {
     if (result[0] != undefined) {
       if (!req.session.uuid) {
         req.session.uuid = result[0]["dataValues"].uuid;
-        console.log(req.session.uuid)
+        console.log(req.session.uuid);
       }
     }
   });
@@ -131,7 +131,7 @@ exports.userLogout = (req, res) => {
       if (err) throw err;
       // console.log('세션 삭제하고 로그아웃됨');
       // console.log("세션값3", req.session);
-      res.redirect("/"); // res.redirect('/public/login.html');
+      res.redirect("/"); // res.redirect('/public/login.ejs');
     });
   } else {
     if (req.session) {
