9600ec472809c217c193f6a5ae4ab63c9f6ef0be
JayGrit <110958755+jaydive@users.noreply.github.com>
2022-09-29 14:29:59 +0900

    done (#170)
    saveresume, elementcareer

--- controller/CuserResume.js
+++ controller/CuserResume.js
@@ -13,31 +13,110 @@ exports.getIntroduce = (req, res) => {
 
 // save introduce
 exports.saveIntroudce = (req, res) => {
-  UserResume.create({
-    uuid: req.session.uuid,
-    stack: req.body.stack,
-    career: req.body.career,
-    portfolio: req.body.portfolio,
-    etc: req.body.etc,
+  UserResume.findOne({
+    attributes: ['uuid'],
+    where: {// id: req.body.id
+      uuid: req.session.uuid}
   })
-  .then((result, err) => {
-    console.log(result)
-  })
-
-  var careerSplit = req.body.career.split('|');
-  var totalCareer = 0;
-
-  for (var i=0; i<careerSplit.length-1; i++){
-    if (i%2 == 0){
-      totalCareer += Number(careerSplit[i]);
+  .then((result)=>{
+    console.log("체크1", result);
+    if (result === null) {
+      UserResume.create({
+        uuid: req.session.uuid,
+        stack: req.body.stack,
+        career: req.body.career,
+        portfolio: req.body.portfolio,
+        etc: req.body.etc
+      })
+      .then((result) => {
+        console.log("최초등록", result)
+        
+        var careerSplit = req.body.career.split('|');
+        var totalCareer = 0;
+        for (var i=0; i<careerSplit.length-1; i++){
+          if (i%2 == 0){
+            totalCareer += Number(careerSplit[i]);
+          }
+        }
+        totalCareer = String(totalCareer);
+        ElementCareer.create({
+          [totalCareer]: 1,
+          id: req.session.uuid
+        })
+        .then((result) => {
+          console.log("커리어요소최초등록"/* , result */)
+        }).catch((err) => {
+          console.log("커리어요소최초등록 Error: ", err);
+        })
+      }).catch((err) => {
+        console.log("최초등록 Error: ", err);
+      })
+    } else {
+      UserResume.update(
+        {
+          uuid: req.session.uuid,
+          stack: req.body.stack,
+          career: req.body.career,
+          portfolio: req.body.portfolio,
+          etc: req.body.etc/* "수정" */
+        },
+        { where: {uuid: req.session.uuid} }
+      )
+      .then((result) => {
+        console.log("수정등록", result) // 수정 갯수
+        
+        var careerSplit = req.body.career.split('|');
+        var totalCareer = 0;
+        for (var i=0; i<careerSplit.length-1; i++){
+          if (i%2 == 0){
+            totalCareer += Number(careerSplit[i]);
+          }
+        }
+        totalCareer = String(totalCareer);
+        ElementCareer.update(
+          {
+            [totalCareer]: 1,
+            id: req.session.uuid
+          },
+          { where: {id: req.session.uuid} }
+        )
+        .then((result) => {
+          console.log("커리어요소수정등록"/* , result */)
+        }).catch((err) => {
+          console.log("커리어요소수정등록 Error: ", err);
+        })
+      }).catch((err) => {
+        console.log("수정등록 Error: ", err);
+      })
     }
-  }
-
-totalCareer = String(totalCareer);
-  ElementCareer.create({
-    [totalCareer] : req.session.uuid,
-    id: req.session.uuid
-  }).then((result) => {
-    // console.log(result)
   })
+
+  // ["dataValue"]
+  // .then((result) => {
+  //   // exist 1 no exist 0 
+  //   if (result[0] != undefined) {
+  //     res.send({result: 1})
+  //   }else{
+  //     res.send({result: 0})
+  //   }
+  // })
+
+  
+
+//   var careerSplit = req.body.career.split('|');
+//   var totalCareer = 0;
+
+//   for (var i=0; i<careerSplit.length-1; i++){
+//     if (i%2 == 0){
+//       totalCareer += Number(careerSplit[i]);
+//     }
+//   }
+
+// totalCareer = String(totalCareer);
+//   ElementCareer.create({
+//     [totalCareer] : req.session.uuid,
+//     id: req.session.uuid
+//   }).then((result) => {
+//     // console.log(result)
+//   })
 }
