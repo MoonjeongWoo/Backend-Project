a1376c6f77a77f31e1c642425403f692a4f19efd
jayGrit <slowt0425@gmail.com>
2022-09-30 16:32:27 +0900

    clean2

--- controller/CjoinMember.js
+++ controller/CjoinMember.js
@@ -82,39 +82,3 @@ exports.idCheckCompany = (req, res) => {
   })
 }
 // -------------------------------
-
-//회원정보 수정 화면
-// exports.readyToEdit = (req, res) => {
-
-//   UserInfo.findOne({
-//     attributes: ['id', "pw", "name", "email", "location"],
-//     where: { uuid: req.session.uuid }
-//   })
-//     .then((result) => {
-//       res.render("editProfile", { nowData: result });
-//       // console.log(data);
-//     })
-// }
-
-// //회원정보 수정 반영
-// exports.editProfile = (req, res) => {
-//   UserInfo.update(
-//     {
-//       id: req.body.id,
-//       pw: req.body.pw,
-//       name: req.body.name,
-//       email: req.body.email,
-//       location: req.body.location
-//     },
-//     { where: { uuid: req.session.uuid } }
-//   )
-// }
-
-// 회원정보 삭제 (탈퇴)
-exports.delAccount = (req, res) => {
-  UserInfo.destroy({where: { id: req.body.id }})
-  // const row = UserInfo.findOne({
-  //   where: { id: req.body.data.id } // 일단 하나만
-  // }).then((row) => { row.destory(); }
-  // )
-}
