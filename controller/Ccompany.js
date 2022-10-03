cdaa3850cc5c061aedb09b5923f6052e56c0fd4c
norii97 <mbhb8817@gmail.com>
2022-10-03 17:19:24 +0900

    accordion 수정

--- controller/Ccompany.js
+++ controller/Ccompany.js
@@ -79,6 +79,9 @@ async function joinQuery(req) {
     stack = data.split("|");
     stack.splice(stack.length - 1);
   }
+  console.log('=======================')
+  console.log(stack)
+  console.log('=======================')
 
   if (stack.length != 0) {
     query += "elementStack AS stack ";
@@ -99,9 +102,9 @@ async function joinQuery(req) {
       query += "elementLocation AS career ";
     } else {
       if (checkElement[0] == 0) {
-        query += "JOIN elementLocation AS location ON career.id = location.id";
+        query += "JOIN elementLocation AS location ON career.id = location.id ";
       } else {
-        query += "JOIN elementLocation AS location ON stack.id = location.id";
+        query += "JOIN elementLocation AS location ON stack.id = location.id ";
       }
     }
     checkElement[2] = 1;
