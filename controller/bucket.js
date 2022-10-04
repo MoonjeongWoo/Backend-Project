496f42c161b4f93af442d82edfcbfc48cc3ab647
jayGrit <slowt0425@gmail.com>
2022-10-04 13:46:56 +0900

    ruleOut done

--- controller/bucket.js
+++ controller/bucket.js
@@ -27,7 +27,8 @@ exports.getViewUserResume = async (req, res) => {
                 stack: result[0].dataValues.stack,
                 career: result[0].dataValues.career,
                 portfolio: result[0].dataValues.portfolio,
-                etc: result[0].dataValues.etc
+                etc: result[0].dataValues.etc,
+                uuid: result[0].dataValues.uuid
             }
             resume[i] = userResume;
         })
@@ -41,19 +42,25 @@ exports.ruleOut = (req, res) => {
         attributes: ['bucket'],
         where: { uuid: req.session.uuid }
     }).then((result) => {
-        console.log("체크체크", typeof result.bucket) // ["047007f5-54f7-41e1-ae75-2cccb17f1c94","64dd76cc-f18a-4308-a70a-a00fb1354a27"]
-        console.log("췍췍2", req.body); // { index: '1' }
         var theBucket = JSON.parse(result.bucket);
-        console.log(typeof req.body.index);
-        if (req.body.index > -1) {
-            theBucket.splice(req.body.index, 1);
+        const isUUID = (element) => element === req.body.uuid;
+        var idx = theBucket.findIndex(isUUID);
+        console.log(idx);
+
+        // console.log(theBucket.findindex(req.body.uuid));
+        // console.log("체크체크", typeof result.bucket) // ["047007f5-54f7-41e1-ae75-2cccb17f1c94","64dd76cc-f18a-4308-a70a-a00fb1354a27"]
+        // console.log("췍췍2", req.body); // { index: '1' }
+        // var theBucket = JSON.parse(result.bucket);
+        // console.log(typeof req.body.index);
+        if (idx > -1) {
+            theBucket.splice(idx, 1);
             console.log("췍췍4", theBucket)
         }
 
-        CompanyInfo.update(
-            {bucket: JSON.stringify(theBucket)},
-            {where: { uuid: req.session.uuid }}
-        )
+        // CompanyInfo.update(
+        //     {bucket: JSON.stringify(theBucket)},
+        //     {where: { uuid: req.session.uuid }}
+        // )
     })
 }
 
